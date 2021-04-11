1efdba82330f174dcd1a825a01ad2ddcd14a73a6
hcedenoindri <hcedenoindriago@gmail.com>
2021-04-11 02:18:02 -0500

    changes to js and index files
    Added main algorithm in js file. Modified static content in index. Renamed css and js files to "project".

---
+++ project.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let projects = [];
+
+    let proj1 = {
+        name: "",
+        desc: "",
+        tools: "",
+        repo: "",
+        url: "",
+        img: "",
+        display: false
+    };
+    projects.push(proj1);
+
+    let proj2 = {
+        name: "",
+        desc: "",
+        tools: "",
+        repo: "",
+        url: "",
+        img: "",
+        display: false        
+    };
+    projects.push(proj2);
+
+    projects.forEach( (proj) => {
+        if (proj.display === true) {
+            // create and append card
+        }
+    });
+
+});
